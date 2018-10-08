aa8a0026d3d66e4fa86ba0f76975cbedee9758ea
N Crozier <nicholas@paradisbasics.com>
2018-10-08 12:13:30 -0700

    weww

--- weather/weather.js
+++ weather/weather.js
@@ -1,24 +1,11 @@
 $(document).ready(function(){
-/*
-  // Get location
-  navigator.geolocation.getCurrentPosition(success, error);
-
-  function success(pos){
-    var lat = pos.coords.latitude;
-    var long = pos.coords.longitude;
-    weather(lat, long);
-  }
 
 
-  function error(){
-    console.log("error");
-  }
 
-*/
 
-  function weather(lat, long){
-    var URL = `https://fcc-weather-api.glitch.me/api/current?lat=37.77&lon=-122.41`;
 
+  function weather(){
+    var URL = "https://fcc-weather-api.glitch.me/api/current?lat=37.77&lon=-122.41";
     $.getJSON(URL, function(data){
       console.log(data);
       updateDOM(data);
@@ -26,18 +13,20 @@ $(document).ready(function(){
     });
   }
 
-  // Update DOM
+  weather();
+
+
   function updateDOM(data) {
 
     var city = data.name;
     var temp = Math.round(data.main.temp);
     var desc = data.weather[0].description;
-    var icon = data.weather[1].icon;
+
 
     $("#city").html(city);
     $("#temp").html(temp);
     $("#desc").html(desc);
-    $("#icon").attr('src', icon);
+    
 
 
   }
