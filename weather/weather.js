5d969b980fa0637ae2b0c6bde132f7b51ac09a05
N Crozier <nicholas@paradisbasics.com>
2018-10-08 11:46:47 -0700

    added weather proj

---
+++ weather/weather.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+
+  // Get location
+  navigator.geolocation.getCurrentPosition(success, error);
+
+  function success(pos){
+    var lat = pos.coords.latitude;
+    var long = pos.coords.longitude;
+    weather(lat, long);
+  }
+
+
+  function error(){
+    console.log("error");
+  }
+
+
+
+  function weather(lat, long){
+    var URL = `https://fcc-weather-api.glitch.me/api/current?lat=${lat}&lon=${long}`;
+
+    $.getJSON(URL, function(data){
+      console.log(data);
+      updateDOM(data);
+
+    });
+  }
+
+  // Update DOM
+  function updateDOM(data) {
+
+    var city = data.name;
+    var temp = Math.round(data.main.temp);
+    var desc = data.weather[0].description;
+    var icon = data.weather[1].icon;
+
+    $("#city").html(city);
+    $("#temp").html(temp);
+    $("#desc").html(desc);
+    $("#icon").attr('src', icon);
+
+
+  }
+
+
+
+
+
+
+
+});
