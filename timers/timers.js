2ac550e9345659452971e2f9d468ba661c5d80b3
N Crozier <nicholas@paradisbasics.com>
2018-10-10 12:01:08 -0700

    timeirs

---
+++ timers/timers.js
@@ -0,0 +1,28 @@
+
+
+var myMessage = document.getElementById("message");
+
+function showMessage() {
+
+  myMessage.className = "show";
+
+}
+
+
+setTimeout(showMessage, 4000);
+
+
+
+
+
+// Getting dance ID
+var dance = document.getElementById("dance");
+
+
+function danceShow() {
+
+  dance.className = "showing";
+
+}
+
+setTimeout(danceShow, 2000);
